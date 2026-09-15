8cfe2372156490c5e855cd2eed34d50295cab190
Pedro Victor <pvmcabral2005@gmail.com>
2026-09-15 09:54:02 -0300

    aulas 10 e 11

---
+++ Aula 11/script.js
@@ -0,0 +1,14 @@
+//let valor = Number(prompt("Digite um valor"));
+//let desconto = valor * 0.15;
+//let valor_final = valor - desconto;
+//console.log("O valor final com o desconto é:" + valor_final);
+//alert("O valor final com desconto é:" + valor_final);
+
+let valor1 = Number(prompt("Digite o primeiro valor"));
+let valor2 = Number(prompt("Digite o segundo valor"));
+
+let divisão = valor1 / valor2;
+console.log("O valor final da operação é:" + divisão);
+
+let distância = Number(prompt("A distância percorrida pela moto em km é:" + km));
+let combustivel = Number(prompt("O combustivel gasto foi de:" + litros));
