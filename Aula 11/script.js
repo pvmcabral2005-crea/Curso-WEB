ede625a1bd245151771b18bc7e8a1b27fb334af4
Pedro Victor <pvmcabral2005@gmail.com>
2026-09-18 11:37:43 -0300

    Novas funcionalidades Js

--- Aula 11/script.js
+++ Aula 11/script.js
@@ -22,25 +22,42 @@
 //let valor_final = valor_produto - desconto;
 
 //alert("O valor final do produto é:" + valor_final);
+let nome = prompt("Qual seu nome");
+let idade = Number(prompt("Qual sua idade:"));
+console.log(nome,idade);
 
 const caixaTexto = document.querySelector('#campoNome');
 const botaoTarde = document.querySelector('#btnTarde');
 const botaoNoite = document.querySelector('#btnNoite');
 const botaoDia = document.querySelector('#btnDia');
 const areaResultado = document.querySelector('#PainelResultado');
-
-botaoTarde.addEventListener('click', function(){
+const corpo = document.querySelector('#corpo');
+const estiloFonte = document.querySelector('#fantastico');
+const botaoTema = document.querySelector('#botaoEscuro');
+botaoTarde.addEventListener('click', function () {
     let Usuario = caixaTexto.value;
     areaResultado.innerHTML = `<h1><em>Boa Tarde ${Usuario}!</em></h1>`;
-});
-botaoDia.addEventListener('click', function(){
-    let Usuario = caixaTexto.value;
-    areaResultado.innerHTML = `<h1><em>Bom Dia ${Usuario}!</em></h1> `
-    corpo.style.backgroundColor = 'green';
+     corpo.style.backgroundColor = 'blue';
+    estiloFonte.style.fontSize = '20px';
+    botaoTarde.classList.add('botao-clicado');
 });
 
-botaoNoite.addEventListener('click', function(){
+botaoDia.addEventListener('click', function () {
+    let Usuario = caixaTexto.value;
+    areaResultado.innerHTML = `<h1><em>Bom Dia ${Usuario}!</em></h1> `;
+     corpo.style.backgroundColor = 'green';
+    estiloFonte.style.fontSize = '20px';
+    botaoDia.classList.add('botao-clicado');
+});
+
+botaoNoite.addEventListener('click', function () {
     let Usuario = caixaTexto.value;
     areaResultado.innerHTML = `<h1><em>Boa Noite ${Usuario}!</em></h1>`;
+    // corpo.style.backgroundColor = 'orange';
+    estiloFonte.style.fontSize = '20px';
+    botaoNoite.classList.add('botao-clicado');
 });
 
+botaoTema.addEventListener('click', function () {
+    corpo.classList.toggle('modo-escuro');
+});
